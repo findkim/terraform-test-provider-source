84f2f97e83a7efd35dc0a72ce9991823ffef1817
terraform-registry-test-user <30361404+terraform-registry-test-user@users.noreply.github.com>
2020-01-23 04:24:38 -0600

    valid module with provider source

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "test" {}
+
+provider "template" {}
+
+terraform {
+  required_providers {
+    null = {
+      version = ">= 2.0.0"
+    }
+    foobar = {
+      source = "partner/foobar"
+      version = "0.1.4"
+    }
+    dne = {
+      source = "custom-registry.io/random/dne"
+    }
+  }
+}
